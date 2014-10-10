d9cf5b924c6bdd79dd820c82a3a41b6fcf872281
Simone <hello@simonewebdesign.it>
2014-10-10 17:48:07 +0100

    Refactoring

--- game.js
+++ game.js
@@ -14,19 +14,13 @@ var ball = {
 }
 
 var p1 = {
-  width: 10,
-  height: 100,
   x: 20,
   y: (canvas.height - 100) / 2,
-  score: 0,
-  speed: 600
+  score: 0
 }
 
 var p2 = {
-  width: 10,
-  height: 100,
   x: canvas.width - 10 - 20,
   y: (canvas.height - 100) / 2,
-  score: 0,
-  speed: 600
+  score: 0
 }
--- reset.js
+++ reset.js
@@ -8,6 +8,18 @@ var reset = function () {
       xVelocity = Math.random() > 0.5 ? 600 : -600, // randomly start from left or right
       yVelocity = 0;
 
+  var paddleSpeed = 600,
+      paddleWidth = 10,
+      paddleHeight = 100;
+
   ball.position = new Vector2(xPosition, yPosition);
   ball.velocity = new Vector2(xVelocity, yVelocity);
+
+  p1.speed = paddleSpeed;
+  p1.width = paddleWidth;
+  p1.height = paddleHeight;
+
+  p2.speed = paddleSpeed;
+  p2.width = paddleWidth;
+  p2.height = paddleHeight;
 }
