a0ab5080ae3c59c32f8aa3bcbe1efca4108ef984
Simone <hello@simonewebdesign.it>
2014-11-22 12:39:44 +0000

    Cleanup: remove unnecessary comments

--- update.js
+++ update.js
@@ -82,39 +82,13 @@ var update = function (modifier) {
     && ball.position.y <= (p1.pos.y + p1.height)
     && p1.pos.y <= (ball.position.y + ball.size)
   ) {
-    // var normal = new Vector2(0, 1);
-    // ball.deflect( normal );
-
-    // Rotate pivot
-    // p1.angle = 5; // note: this shouldn't really be necessary! Because the angle is implicitly stored in the direction vector.
-    // var angle = 2;
-    // console.log("initial pivot:", p1.pivot);
-
-    // // var pivot = p1.pivot.clone().normalize();
-    // console.log("pivot (normalized):", pivot);
-
-    // var rotatedPivot = pivot.rotate(angle);
-    // console.log("rotatedPivot:", rotatedPivot);
-
-
-
-    // p1.pivot.rotate(p1.angle);
-    // p1.rotateAroundPivot();
-
     // limits
     var limit = p1.pos.x + p1.width;
-    // console.log("limit: ", limit);
-    // console.log("ball position:", ball.position);
 
     if (ball.position.x <= limit) {
       ball.position.x = limit + 1;
     }
-    // console.log("again, ball position:", ball.position);
 
-
-
-
-    ///////////////////////////////////////
     // First of all, I need to calculate the wall normal properly.
     // I start by getting the A and B points:
     var a = p1.points[1].clone(),
@@ -123,21 +97,7 @@ var update = function (modifier) {
     // Then I get the directing vector:
     p1.direction = b.subSelf(a).normalize();
 
-    // console.log("dirVector:", dirVector);
-
-    // And I flip it:
-    // var dirVectorFlipped = dirVector.flipRight();
-    // console.log("dirVectorFlipped:", dirVectorFlipped);
-
-    // norm is paddle's normalized directing vector.
-    // could store this in paddle.direction.
-    // this should be calculated using cross product.
-    // var norm = new Vector2(0, 1);
-    // var norm = dirVectorFlipped.normalize();
-
-    // p1.rotatePoints(p1.angle);
     ball.deflect(p1.direction);
-    ///////////////
   }
 
   // Ball is colliding with P2
@@ -148,29 +108,6 @@ var update = function (modifier) {
     && p2.pos.y <= (ball.position.y + ball.size)
   ) {
 
-    // var a = ball.centerRight(),
-    //     b = p2.centerLeft(),
-    //// you could use distanceTo() here.
-    //     distanceBetweenTwoPoints = b.subSelf(a).length(), // will be used as angle
-    //     pivot = p2.bottom();
-
-    // // FIXME: the actual logic error is inside directingVector() function.
-    // // maybe the correct logic is:
-    // if (a.y > b.y) {
-    //   // collided after the center (on the left of the paddle)
-
-    // } else {
-    //   // collided before the center (on the right of the paddle)
-    // }
-
-    // var hitFactor = 0.2;
-    // ball.deflect( new Vector2(hitFactor, 1) );
-    // ball.deflect( p2.calculateDirectingVector(angle) ); // the new one
-    // ball.deflect( p2.directingVector() ); // should be new Vector2(0, 1)
-    // normal is paddle's normalized directing vector.
-    // could store this in paddle.direction.
-    // this should be calculated using cross product.
-    // var normal = p2.direction.clone();
     ball.deflect( new Vector2(0,1) );
   }
 
@@ -187,6 +124,4 @@ var update = function (modifier) {
   // Ball movement
   ball.position.x += ball.velocity.x * modifier;
   ball.position.y += ball.velocity.y * modifier;
-
-  // p1.rotatePoints(2);
 };
