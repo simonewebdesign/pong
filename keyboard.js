d69a5b6049b1f0eb772526587d14cbdb9d8a8c1d
Simone <hello@simonewebdesign.it>
2014-11-25 09:36:12 +0000

    Fix bug on keyboard

--- keyboard.js
+++ keyboard.js
@@ -3,32 +3,40 @@ var keysDown = {};
 var ROTATION_ANGLE = 5;
 
 addEventListener("keydown", function (e) {
-  keysDown[e.keyCode] = true;
 
   if (e.keyCode == 87) { // P1 pressed up (key: w)
-    p1.angle = ROTATION_ANGLE;
-    p1.rotatePoints(p1.angle);
+    if (!keysDown[87]) { // execute only the first time
+      p1.angle = ROTATION_ANGLE;
+      p1.rotatePoints(p1.angle);
+    }
   }
 
   if (e.keyCode == 83) { // P1 pressed down (key: s)
-    p1.angle = -ROTATION_ANGLE;
-    p1.rotatePoints(p1.angle);
+    if (!keysDown[83]) { // execute only the first time
+      p1.angle = -ROTATION_ANGLE;
+      p1.rotatePoints(p1.angle);
+    }
   }
 
   if (e.keyCode == 38) { // P2 pressed up (key: arrow up)
-    p2.angle = ROTATION_ANGLE;
-    p2.rotatePoints(p2.angle);
+    if (!keysDown[38]) { // execute only the first time
+      p2.angle = ROTATION_ANGLE;
+      p2.rotatePoints(p2.angle);
+    }
   }
 
   if (e.keyCode == 40) { // P2 pressed down (key: arrow down)
-    p2.angle = -ROTATION_ANGLE;
-    p2.rotatePoints(p2.angle);
+    if (!keysDown[40]) { // execute only the first time
+      p2.angle = -ROTATION_ANGLE;
+      p2.rotatePoints(p2.angle);
+    }
   }
+
+  keysDown[e.keyCode] = true;
 
 }, false);
 
 addEventListener("keyup", function (e) {
-  delete keysDown[e.keyCode];
 
   if (e.keyCode == 87) { // P1 released up (key: w)
     p1.angle = 0;
@@ -49,5 +57,7 @@ addEventListener("keyup", function (e) {
     p2.angle = 0;
     p2.realignPoints();
   }
+
+  delete keysDown[e.keyCode];
 
 }, false);
