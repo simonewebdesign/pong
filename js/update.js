650a599b516540b46b0c2a2ee83451d70fdc4eca
Simone <hello@simonewebdesign.it>
2014-12-01 13:14:34 +0000

    Fix positioning issues once and for all
    Also removing console.log calls

--- js/update.js
+++ js/update.js
@@ -14,56 +14,66 @@ var update = function (modifier) {
   }
 
   if (87 in keysDown) { // P1 holding up (key: w)
-    // Update position
-    var movementYaxis = p1.speed * modifier;
-    p1.pos.y -= movementYaxis;
+    if (p1.pos.y > 0) {
+      // Update position
+      var movementYaxis = p1.speed * modifier;
+      p1.pos.y -= movementYaxis;
 
-    p1.updatePivot();
-    p1.updatePoints(movementYaxis * -1);
+      p1.updatePivot();
+      p1.updatePoints(movementYaxis * -1);
 
-    if (p1.pos.y <= 0) {
-      p1.pos.y = 0;
+      // go back on track, just in case it went too out of boundaries
+      if (p1.pos.y <= 0) {
+        p1.pos.y = 0;
+      }
     }
   }
 
   if (83 in keysDown) { // P1 holding down (key: s)
-    // Update position
-    var movementYaxis = p1.speed * modifier;
-    p1.pos.y += movementYaxis;
+    if (p1.pos.y + p1.height < canvas.height) {
+      // Update position
+      var movementYaxis = p1.speed * modifier;
+      p1.pos.y += movementYaxis;
 
-    p1.updatePivot();
-    p1.updatePoints(movementYaxis);
+      p1.updatePivot();
+      p1.updatePoints(movementYaxis);
 
-    var limit = canvas.height - p1.height;
-    if (p1.pos.y >= limit) {
-      p1.pos.y = limit;
+      // go back on track, just in case it went too out of boundaries
+      if (p1.pos.y + p1.height > canvas.height) {
+        p1.pos.y = canvas.height - p1.height;
+      }
     }
   }
 
   if (38 in keysDown) { // P2 holding up
-    // Update position
-    var movementYaxis = p2.speed * modifier;
-    p2.pos.y -= movementYaxis;
+    if (p2.pos.y > 0) {
+      // Update position
+      var movementYaxis = p2.speed * modifier;
+      p2.pos.y -= movementYaxis;
 
-    p2.updatePivot();
-    p2.updatePoints(movementYaxis * -1);
+      p2.updatePivot();
+      p2.updatePoints(movementYaxis * -1);
 
-    if (p2.pos.y <= 0) {
-      p2.pos.y = 0;
+      // go back on track, just in case it went too out of boundaries
+      if (p2.pos.y <= 0) {
+        p2.pos.y = 0;
+      }
     }
   }
 
   if (40 in keysDown) { // P2 holding down
-    // Update position
-    var movementYaxis = p2.speed * modifier;
-    p2.pos.y += movementYaxis;
+    if (p2.pos.y + p2.height < canvas.height) {
+      // Update position
+      var movementYaxis = p2.speed * modifier;
+      p2.pos.y += movementYaxis;
 
-    p2.updatePivot();
-    p2.updatePoints(movementYaxis);
+      p2.updatePivot();
+      p2.updatePoints(movementYaxis);
 
-    var limit = canvas.height - p2.height;
-    if (p2.pos.y >= limit) {
-      p2.pos.y = limit;
+      // go back on track, just in case it went too out of boundaries
+      if (p2.pos.y + p2.height > canvas.height) {
+        p2.pos.y = canvas.height - p2.height;
+      }
     }
   }
 
@@ -90,12 +100,6 @@ var update = function (modifier) {
     && ball.position.y <= (p1.pos.y + p1.height)
     && p1.pos.y <= (ball.position.y + ball.size)
   ) {
-    // limits
-    var limit = p1.pos.x + p1.width;
-    if (ball.position.x <= limit) {
-      ball.position.x = limit + 1;
-    }
-
     // First of all, I need to calculate the wall normal properly.
     // I start by getting the A and B points:
     var a = p1.points[1].clone(),
@@ -104,8 +108,11 @@ var update = function (modifier) {
     // Then I get the directing vector:
     p1.direction = b.subSelf(a).normalize();
 
-    console.log(p1.direction);
+    // Finally I reflect the ball
     ball.deflect(p1.direction);
+
+    // go back on track, just in case it went too out of boundaries
+    ball.position.x = p1.pos.x + p1.width +1;
 
     paddleHitSoundLow();
   }
@@ -117,22 +124,20 @@ var update = function (modifier) {
     && ball.position.y <= (p2.pos.y + p2.height)
     && p2.pos.y <= (ball.position.y + ball.size)
   ) {
-    // limits
-    var limit = p2.pos.x;
-    if (ball.position.x >= limit) {
-      ball.position.x = limit - 1;
-    }
-
     // First of all, I need to calculate the wall normal properly.
     // I start by getting the A and B points:
     var a = p2.points[0].clone(),
         b = p2.points[3].clone();
 
-    // Then I get the directing vector:
+    // Then I get the directing vector
     p2.direction = b.subSelf(a).normalize();
 
-    console.log(p2.direction);
+    // Finally I reflect the ball
     ball.deflect(p2.direction);
+
+    // go back on track, just in case it went too out of boundaries
+    ball.position.x = p2.pos.x - ball.size -1;
+    console.log("ball pos:", ball.position.x, "p2 pos:", p2.pos.x)
 
     paddleHitSoundHigh();
   }
@@ -140,11 +145,17 @@ var update = function (modifier) {
   // Ball is colliding with the top
   if (ball.position.y <= 0) {
     ball.deflect( new Vector2(1, 0) );
+
+    // go back on track, just in case it went too out of boundaries
+    ball.position.y = 0.1;
   }
 
   // Ball is colliding with the bottom
   if (ball.position.y + ball.size >= canvas.height) {
     ball.deflect( new Vector2(1, 0) );
+
+    // go back on track, just in case it went too out of boundaries
+    ball.position.y = canvas.height - ball.size -1;
   }
 
   // Ball movement
